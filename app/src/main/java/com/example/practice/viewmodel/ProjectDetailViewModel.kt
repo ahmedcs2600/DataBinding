a9887749a9cbc7b256ec5c01658d159222c3e7ec
HP Invsion 001 <invision001@invisionsolutions.com>
2020-03-30 19:42:53 +0500

    minor changes

--- app/src/main/java/com/example/practice/viewmodel/ProjectDetailViewModel.kt
+++ app/src/main/java/com/example/practice/viewmodel/ProjectDetailViewModel.kt
@@ -52,7 +52,6 @@ class ProjectDetailViewModel() : BaseViewModel() {
         super.onCleared()
         with(compositeDisposable){
             clear()
-            dispose()
         }
     }
 }
--- app/src/main/java/com/example/practice/viewmodel/ProjectListViewModel.kt
+++ app/src/main/java/com/example/practice/viewmodel/ProjectListViewModel.kt
@@ -43,7 +43,6 @@ class ProjectListViewModel : BaseViewModel(){
         super.onCleared()
         with(compositeDisposable) {
             clear()
-            dispose()
         }
     }
 }
